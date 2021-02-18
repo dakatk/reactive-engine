bcc5dcb6ea4dac078f91ebeb651815ab8c40a97a
dakatk <dakatk97@gmail.com>
2021-02-18 10:23:24 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const App = new BigBrother({
+    watchers: {
+        title: 'Title',
+        titleClass: 'red',
+        titleId: 'bg-yellow',
+        showTitle: true,
+        text: ['Zero', 'One', 'Two']
+    },
+    listeners: {
+        switchClass: function () {
+            const titleClass = this.watchers.titleClass;
+            this.watchers.titleClass = titleClass === 'red' ? 'blue' : 'red';
+        },
+        switchId: function () {
+            const titleId = this.watchers.titleId;
+            this.watchers.titleId = titleId === 'bg-green' ? 'bg-yellow' : 'bg-green';
+        },
+        toggleTitle: function (event) {
+            const showTitle = event.target.checked;
+            this.watchers.showTitle = showTitle;
+        }
+    }
+});
