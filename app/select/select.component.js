2d02501e73fa6041a1d7beee5de85c77c5a7c66e
dakatk <dakatk97@gmail.com>
2021-12-30 23:27:06 -0600

    Removed children component reference

--- app/select/select.component.js
+++ app/select/select.component.js
@@ -1,5 +1,4 @@
 import PartyMember from '../../lib/party-member';
-import todaysDate from './todays-date';
 
 const SelectComponent = new PartyMember(
     'select-component',
@@ -8,9 +7,6 @@ const SelectComponent = new PartyMember(
     },
     {
         updateText: function(event) {
-            const dateComponent = this.children.example;
-
-            dateComponent.watchers.date = todaysDate();
             this.watchers.text[0] = event.target.value;
         }
     },
