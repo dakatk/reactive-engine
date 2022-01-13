5bd6f8b1c00d12b1237f17d7a64045fd1996efc4
dakatk <dakatk97@gmail.com>
2022-01-13 14:18:51 -0600

    Removed example-app from master branch

--- example-app/app/app-component.js
+++
@@ -1,5 +0,0 @@
-import PartyMember from 'ingsoc-js/party-member';
-
-const AppComponent = new PartyMember('app-component', {message: 'Hello, World!'});
-
-export default AppComponent;
--- example-app/app/date/date-component.js
+++
@@ -1,5 +0,0 @@
-import PartyMember from 'ingsoc-js/party-member';
-
-const DateComponent = new PartyMember('date-component', { date: '2021-02-01' });
-
-export default DateComponent;
