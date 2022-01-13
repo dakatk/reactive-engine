8f8efa67a622dd4fb8833cd73553896635a8e1cf
dakatk <dakatk97@gmail.com>
2022-01-12 18:11:16 -0600

    Added MIME types

--- ingsoc-js/lib/cli/serve-logic/serve-file.js
+++ ingsoc-js/lib/cli/serve-logic/serve-file.js
@@ -1,33 +1,22 @@
 import handleError from './handle-error.js';
+import mimetypes from './mimetypes.js';
 import path from 'path';
 import fs from 'fs';
-
-const CONTENT_TYPE = {
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.html': 'text/html',
-    '.ico': 'image/x-icon'
-};
 
 const PUBLIC_FILES = ['index.html', 'bundle.js', 'style.css'];
 
 export default function serveFile({url}, res, {outputDirectory, staticDirectory}) {
-    if (url === '/') {
-        url = 'index.html';
-    }
-    if (url.charAt(0) === '/') {
-        url = url.substring(1);
-    }
-    const directory = PUBLIC_FILES.includes(url) ?
+    const file = toFileName(url);
+    const directory = PUBLIC_FILES.includes(file) ?
         outputDirectory :
         staticDirectory;
 
-    const extName = path.extname(url);
-    const contentType = CONTENT_TYPE[extName];
+    const extName = path.extname(file);
+    const contentType = mimetypes[extName];
     if (!contentType) {
         handleError(res, 403, extName);
     }
-    fs.readFile(path.resolve(directory, url), (err, content) => {
+    fs.readFile(path.resolve(directory, file), (err, content) => {
         if (err) {
             const code = err.code === 'ENOENT' ? 404 : 500;
             const details = err.message;
@@ -36,4 +25,14 @@ export default function serveFile({url}, res, {outputDirectory, staticDirectory}
         res.writeHead(200, { 'Content-Type': contentType });
         res.end(content, 'utf-8');
     });
+}
+
+function toFileName(url) {
+    if (url === '/') {
+        return 'index.html';
+    }
+    if (url.charAt(0) === '/') {
+        url = url.substring(1);
+    }
+    return url;
 }
